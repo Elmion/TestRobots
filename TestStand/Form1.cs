b88e6517139f6a6fef7ab941937995f3dede1f34
Elmion <iight@bk.ru>
2018-03-13 15:19:06 +1000

    23123123123

--- TestStand/Form1.cs
+++ TestStand/Form1.cs
@@ -14,50 +14,113 @@ namespace TestStand
 {
     public partial class Form1 : Form
     {
+        public static readonly Random rnd = new Random();
+       
         bool LefButton = false;
-        const int SIZE_BUTTON = 30;
+        const int SIZE_BUTTON = 40;
         List<List<Button>> field;
-        LineTemplate asseblity;
+        List<Button> Tail;
+        LineTemplate assebler;
+        Button Prev = null;
+        Button Current = null;
+        Graphics g;
+        Panel LinePanel;
         public Form1()
         {
             InitializeComponent();
+            InitTemplates();
+            g = pField.CreateGraphics();
             pField.MouseDown += PField_MouseDown;
+            pField.MouseUp += PField_MouseUp;
             pField.MouseMove += PField_MouseMove;
             field = new List<List<Button>>();
-
+            Tail = new List<Button>();
             for (int i = 0; i < 6; i++)
             {
                 field.Add(new List<Button>());
                 for (int j = 0; j < 6; j++)
                 {
-                    Button b = new Button();
-                    field[i].Add(b);
-                    b.Parent = pField;
-                    b.Location = new Point(i * SIZE_BUTTON, j * SIZE_BUTTON);
-                    b.Size = new Size(SIZE_BUTTON, SIZE_BUTTON);
-                    b.BackColor = Color.Red;
-                    b.Enabled = false;
+                    field[i].Add(CreateButton(i,j,rnd.Next(3)));
                 }
             }
         }
-
+        private void InitTemplates()
+        {
+            assebler = new LineTemplate();
+            assebler.AddTemplate("ore/ore");
+            assebler.AddTemplate("ore/cuprum");
+            assebler.AddTemplate("ore/cuprum/ore/cuprum/oil");
+            assebler.AddTemplate("ore/cuprum/ore/tail");
+        }
         private void PField_MouseMove(object sender, MouseEventArgs e)
         {
-            if (LefButton)
+            if (LefButton )
                  {
                      int x = ((e.Location.X - e.Location.X % SIZE_BUTTON) / SIZE_BUTTON);
                      int y = ((e.Location.Y - e.Location.Y % SIZE_BUTTON) / SIZE_BUTTON);
-                    if(x >= 0 && x < field.Count && y >= 0 && y < field.Count)
-                                                    field[x][y].BackColor = Color.Blue;
+
+                      
+                        if (x >= 0 && x < field.Count && y >= 0 && y < field.Count)
+                        {
+                            if (Tail.Last() != field[x][y])
+                            {
+                                if (Tail.Count > 1 && field[x][y] == Tail[Tail.Count - 2])
+                                {
+                                    Tail.Last().BackColor = Color.Gray;
+                                    Tail.Remove(Tail.Last());
+                                    assebler.Preverios();
+                                }
+                                else
+                                {
+                                    field[x][y].BackColor = Color.Red;
+                                    Tail.Add(field[x][y]);
+                                    switch ((int)field[x][y].Tag)
+                                    {
+                                        case 0: assebler.Next("ore"); break;
+                                        case 1: assebler.Next("cupper"); ; break;
+                                        case 2: assebler.Next("oil"); break;
+                                    }
+                           
+                                }
+                            }
+                            PrintTail();
+                        }
                  }
         }
-
         private void PField_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
+            {
                 LefButton = true;
-            else
+                int x = ((e.Location.X - e.Location.X % SIZE_BUTTON) / SIZE_BUTTON);
+                int y = ((e.Location.Y - e.Location.Y % SIZE_BUTTON) / SIZE_BUTTON);
+                Current = field[x][y];
+                field[x][y].BackColor = Color.Red;
+                Tail.Add(field[x][y]);
+                switch ((int)field[x][y].Tag)
+                {
+                    case 0: assebler.Next("ore"); break;
+                    case 1: assebler.Next("cupper"); ; break;
+                    case 2: assebler.Next("oil"); break;
+                }
+            }
+        }
+        private void PField_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
                 LefButton = false;
+                assebler.EndLine();
+                for (int i = 0; i < 6; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        field[i][j].BackColor = Color.Gray;
+                    }
+                }
+                Console.Clear();
+                Tail.Clear();
+            }
         }
         private void B_MouseEnter(object sender, EventArgs e)
         {
@@ -66,6 +129,53 @@ namespace TestStand
         private void Test(object sender, EventArgs e)
         {
             
+        }
+        void InputOUT(string s)
+        {
+            assebler.Next(s);
+            Console.Clear();
+            Console.WriteLine(assebler.Cicles);
+        }
+        void PrintTail()
+        {
+            Console.Clear();
+            foreach (var item in Tail)
+            {
+                switch ((int)item.Tag)
+                    {
+                        case 0: Console.Write("Ore/"); break;
+                        case 1: Console.Write("Cupper/"); break;
+                        case 2: Console.Write("Oil/"); break;
+                    }
+            }
+            Console.WriteLine();
+            Console.WriteLine(assebler.Cicles);
+        }
+
+
+
+    Button CreateButton(int x,int y,int TypeButton)
+        {
+            Button b = new Button();
+
+            b.Parent = pField;
+            b.Location = new Point(x * SIZE_BUTTON, y* SIZE_BUTTON);
+            b.Size = new Size(SIZE_BUTTON, SIZE_BUTTON);
+            b.BackColor = Color.Gray;
+            b.Enabled = false;
+            b.Tag = TypeButton;
+            Panel p = new Panel();
+            p.Parent = b;
+            p.Size = new Size(b.Size.Height - 12, b.Size.Width - 12);
+            p.BackgroundImageLayout = ImageLayout.Stretch;
+            p.Location = new Point(6, 6);
+
+
+            if (TypeButton == 0) p.BackgroundImage = Properties.Resources._1;
+            if (TypeButton == 1) p.BackgroundImage = Properties.Resources._2;
+            if (TypeButton == 2) p.BackgroundImage = Properties.Resources._3;
+
+            return b;
         }
     }
 }
--- TestStand/TileControl.Designer.cs
+++ TestStand/TileControl.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace TestStand
 {
-    partial class Form1
+    partial class TileControl
     {
-        /// <summary>
+        /// <summary> 
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Освободить все используемые ресурсы.
         /// </summary>
         /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
@@ -20,42 +20,39 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Код, автоматически созданный конструктором компонентов
 
-        /// <summary>
+        /// <summary> 
         /// Требуемый метод для поддержки конструктора — не изменяйте 
         /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            this.pField = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // pField
+            // button1
             // 
-            this.pField.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pField.Cursor = System.Windows.Forms.Cursors.Default;
-            this.pField.Location = new System.Drawing.Point(12, 12);
-            this.pField.Margin = new System.Windows.Forms.Padding(0);
-            this.pField.Name = "pField";
-            this.pField.Size = new System.Drawing.Size(349, 312);
-            this.pField.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 144);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // TileControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(373, 336);
-            this.Controls.Add(this.pField);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Controls.Add(this.button1);
+            this.Name = "TileControl";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel pField;
+        private System.Windows.Forms.Button button1;
     }
 }
-
